1cf6ab35be6752ac935e4ba24f769622aba14383
davidlucid <davidlucidsl@gmail.com>
2020-06-30 21:30:22 -0500

    Improved display of dates on charts

--- src/js/app.js
+++ src/js/app.js
@@ -225,44 +225,16 @@ App = {
           scales: {
             xAxes: [{
               type: 'time',
+              time: {
+                unit: 'day',
+                tooltipFormat: 'LL'
+              },
               distribution: 'series',
               offset: true,
               ticks: {
-                major: {
-                  enabled: true,
-                  fontStyle: 'bold'
-                },
-                source: 'data',
                 autoSkip: true,
-                autoSkipPadding: 75,
-                maxRotation: 0,
-                sampleSize: 100
-              },
-              afterBuildTicks: function(scale, ticks) {
-                var majorUnit = scale._majorUnit;
-                var firstTick = ticks[0];
-                var i, ilen, val, tick, currMajor, lastMajor;
-
-                val = moment(ticks[0].value);
-                if ((majorUnit === 'minute' && val.second() === 0)
-                    || (majorUnit === 'hour' && val.minute() === 0)
-                    || (majorUnit === 'day' && val.hour() === 9)
-                    || (majorUnit === 'month' && val.date() <= 3 && val.isoWeekday() === 1)
-                    || (majorUnit === 'year' && val.month() === 0)) {
-                  firstTick.major = true;
-                } else {
-                  firstTick.major = false;
-                }
-                lastMajor = val.get(majorUnit);
-
-                for (i = 1, ilen = ticks.length; i < ilen; i++) {
-                  tick = ticks[i];
-                  val = moment(tick.value);
-                  currMajor = val.get(majorUnit);
-                  tick.major = currMajor !== lastMajor;
-                  lastMajor = currMajor;
-                }
-                return ticks;
+                autoSkipPadding: 20,
+                maxRotation: 0
               }
             }],
             yAxes: [{
@@ -352,44 +324,16 @@ App = {
           scales: {
             xAxes: [{
               type: 'time',
+              time: {
+                unit: 'day',
+                tooltipFormat: 'LL'
+              },
               distribution: 'series',
               offset: true,
               ticks: {
-                major: {
-                  enabled: true,
-                  fontStyle: 'bold'
-                },
-                source: 'data',
                 autoSkip: true,
-                autoSkipPadding: 75,
-                maxRotation: 0,
-                sampleSize: 100
-              },
-              afterBuildTicks: function(scale, ticks) {
-                var majorUnit = scale._majorUnit;
-                var firstTick = ticks[0];
-                var i, ilen, val, tick, currMajor, lastMajor;
-
-                val = moment(ticks[0].value);
-                if ((majorUnit === 'minute' && val.second() === 0)
-                    || (majorUnit === 'hour' && val.minute() === 0)
-                    || (majorUnit === 'day' && val.hour() === 9)
-                    || (majorUnit === 'month' && val.date() <= 3 && val.isoWeekday() === 1)
-                    || (majorUnit === 'year' && val.month() === 0)) {
-                  firstTick.major = true;
-                } else {
-                  firstTick.major = false;
-                }
-                lastMajor = val.get(majorUnit);
-
-                for (i = 1, ilen = ticks.length; i < ilen; i++) {
-                  tick = ticks[i];
-                  val = moment(tick.value);
-                  currMajor = val.get(majorUnit);
-                  tick.major = currMajor !== lastMajor;
-                  lastMajor = currMajor;
-                }
-                return ticks;
+                autoSkipPadding: 20,
+                maxRotation: 0
               }
             }],
             yAxes: [{
