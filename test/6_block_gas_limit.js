4a3c043cfdca9147c2b1050ba31216cbdf4cd1a5
davidlucid <davidlucidsl@gmail.com>
2020-08-14 15:37:41 -0500

    Added test to check gas used by RariFundController.upgradeFundController

---
+++ test/6_block_gas_limit.js
@@ -0,0 +1,53 @@
+const erc20Abi = require('./abi/ERC20.json');
+
+const currencies = require('./fixtures/currencies.json');
+const pools = require('./fixtures/pools.json');
+
+const RariFundController = artifacts.require("RariFundController");
+const RariFundManager = artifacts.require("RariFundManager");
+
+// These tests expect the owner and the fund rebalancer of RariFundController and RariFundManager to be set to process.env.DEVELOPMENT_ADDRESS
+contract("RariFundController", accounts => {
+  it("should put upgrade the FundController with funds in all pools in all currencies without using too much gas", async () => {
+    let fundControllerInstance = await RariFundController.deployed();
+    let fundManagerInstance = await RariFundManager.deployed();
+
+    // Check balance before deposits
+    let oldRawFundBalance = await fundManagerInstance.getRawFundBalance.call();
+
+    // Tally up USD deposited
+    var totalUsdBN = web3.utils.toBN(0);
+    
+    // For each currency of each pool, deposit to fund and deposit to pool
+    for (const poolName of Object.keys(pools)) for (const currencyCode of Object.keys(pools[poolName].currencies)) {
+      // Approve and deposit tokens to the fund
+      var amountBN = web3.utils.toBN(10 ** (currencies[currencyCode].decimals - 1));
+      totalUsdBN.iadd(web3.utils.toBN(1e17));
+      var erc20Contract = new web3.eth.Contract(erc20Abi, currencies[currencyCode].tokenAddress);
+      await erc20Contract.methods.approve(RariFundManager.address, amountBN.toString()).send({ from: process.env.DEVELOPMENT_ADDRESS });
+      await fundManagerInstance.deposit(currencyCode, amountBN, { from: process.env.DEVELOPMENT_ADDRESS });
+
+      // Approve and deposit to pool
+      await fundControllerInstance.approveToPool(["dYdX", "Compound", "Aave"].indexOf(poolName), currencyCode, amountBN, { from: process.env.DEVELOPMENT_ADDRESS });
+      await fundControllerInstance.depositToPool(["dYdX", "Compound", "Aave"].indexOf(poolName), currencyCode, amountBN, { from: process.env.DEVELOPMENT_ADDRESS });
+    }
+
+    // Disable original FundController and FundManager
+    await fundControllerInstance.disableFund({ from: process.env.DEVELOPMENT_ADDRESS });
+    await fundManagerInstance.disableFund({ from: process.env.DEVELOPMENT_ADDRESS });
+
+    // Create new FundController and set its FundManager
+    var newFundControllerInstance = await RariFundController.new({ from: process.env.DEVELOPMENT_ADDRESS });
+    await newFundControllerInstance.setFundManager(RariFundManager.address, { from: process.env.DEVELOPMENT_ADDRESS });
+
+    // Upgrade!
+    var result = await fundControllerInstance.upgradeFundController(newFundControllerInstance.address, { from: process.env.DEVELOPMENT_ADDRESS });
+    console.log("Gas usage of RariFundController.upgradeFundController:", result.receipt.gasUsed);
+    assert.isAtMost(result.receipt.gasUsed, 5000000); // Assert it uses no more than 5 million gas
+    var result = await fundManagerInstance.setFundController(newFundControllerInstance.address, { from: process.env.DEVELOPMENT_ADDRESS });
+
+    // Check balance of FundManager
+    let newRawFundBalance = await fundManagerInstance.getRawFundBalance.call();
+    assert(newRawFundBalance.gte(oldRawFundBalance.add(totalUsdBN.mul(web3.utils.toBN(9999)).div(web3.utils.toBN(10000)))));
+  });
+});
