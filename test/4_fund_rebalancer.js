39d6879d5b64068169ad3674f04f55e87edaee51
davidlucid <davidlucidsl@gmail.com>
2020-09-10 13:33:03 -0500

    Added copyright notices to tests

--- test/4_fund_rebalancer.js
+++ test/4_fund_rebalancer.js
@@ -1,3 +1,12 @@
+/**
+ * COPYRIGHT © 2020 RARI CAPITAL, INC. ALL RIGHTS RESERVED.
+ * Anyone is free to integrate the public APIs (described in `API.md` of the `rari-contracts` package) of the official smart contract instances deployed by Rari Capital, Inc. in any application (commercial or noncommercial and under any license) benefitting Rari Capital, Inc.
+ * Only those with explicit permission from a co-founder of Rari Capital (Jai Bhavnani, Jack Lipstone, or David Lucid) are permitted to study, review, or analyze any part of the source code contained in the `rari-contracts` package.
+ * Reuse (including deployment of smart contracts other than private testing on a private network), modification, redistribution, or sublicensing of any source code contained in the `rari-contracts` package is not permitted without the explicit permission of David Lucid of Rari Capital, Inc.
+ * No one is permitted to use the software for any purpose other than those allowed by this license.
+ * This license is liable to change at any time at the sole discretion of David Lucid of Rari Capital, Inc.
+ */
+
 const erc20Abi = require('./abi/ERC20.json');
 const mAssetValidationHelperAbi = require('./abi/MassetValidationHelper.json');
 
