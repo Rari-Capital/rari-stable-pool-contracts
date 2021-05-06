386aa8811e7f12c2908066ae17af923758503739
davidlucid <davidlucidsl@gmail.com>
2021-05-06 11:35:43 -0500

    SPDX

--- contracts/external/aave/LendingPool.sol
+++ contracts/external/aave/LendingPool.sol
@@ -1,10 +1,4 @@
-/**
- * Aave Protocol
- * Copyright (C) 2019 Aave
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details
- */
-
+// SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.5.17;
 
 /**
--- contracts/external/mstable/MassetStructs.sol
+++ contracts/external/mstable/MassetStructs.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
 pragma solidity 0.5.17;
 
 interface MassetStructs {
